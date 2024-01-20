bfd7b4fc3486ce2952fb95dce4847d6c7e4acdfd
Ryan Hammer <ryanhammer1@protonmail.com>
2024-01-20 15:48:07 -0500

    formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,44 @@
 import Image from 'next/image';
-import homePageBannerImage from '../../public/assets/images/home-page-banner.png';
+import homePageBannerImage from '../public/assets/images/home-page-banner.png';
 import { Button } from './components/button';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <Button className="fixed left-0 top-0" size="md" href="/join">
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
+        <Button className='fixed left-0 top-0' size='md' href='/join'>
           Sign Up
         </Button>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <Button size="md" href="/docs/getting-started">
+        <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none'>
+          <Button size='md' href='/docs/getting-started'>
             Log In
           </Button>
         </div>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative"
-          src={homePageBannerImage}
-          alt="Next.js Logo"
-          priority
-        />
+        <Image className='relative' src={homePageBannerImage} alt='Next.js Logo' priority />
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+      <div className='mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left'>
+        <div className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'>
           <h2 className={`mb-3 text-2xl font-semibold`}>Recipe Box </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Create and store all your favorite recipes.
-          </p>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Create and store all your favorite recipes.</p>
         </div>
 
-        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+        <div className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'>
           <h2 className={`mb-3 text-2xl font-semibold`}>Meal Planner </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Generate weekly meal plans from your stored recipes.
-          </p>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Generate weekly meal plans from your stored recipes.</p>
         </div>
 
-        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+        <div className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'>
           <h2 className={`mb-3 text-2xl font-semibold`}>Pantry </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Track the ingredients you have, including the amount of each and
-            when they expire.
+            Track the ingredients you have, including the amount of each and when they expire.
           </p>
         </div>
 
-        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+        <div className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'>
           <h2 className={`mb-3 text-2xl font-semibold`}>Shopping List </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Generate a shopping list based on your meal plan and food inventory.
